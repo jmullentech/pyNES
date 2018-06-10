0f8daf0feedffe4f197440a4b82ece2401f60b4d
John Mullen <jmullentech@gmail.com>
2018-06-10 15:44:49 -0400

    Creates parseable list of ROMs in all sub-directories

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python2.7
 # encoding: utf-8
 
-import sys, os, npyscreen, curses
+import sys, os
+import fnmatch
+import npyscreen, curses
+
+rompath = '/home/john/NES/ROMs'
+
+gameList = []
+for root, dirnames, filenames in os.walk(rompath):
+    for filename in fnmatch.filter(filenames, '*.nes'):
+        gameList.append(os.path.join(root, filename))
 
 class App(npyscreen.StandardApp):
     def onStart(self):
@@ -15,13 +24,14 @@ class Browser(npyscreen.BoxTitle):
 
 class MainForm(npyscreen.FormBaseNew):
     def create(self):
-
+        
         y, x = self.useable_space()
 
-        self.InstructText = self.add(InstructText, name="Instructions", value = "Use the NES controller's D-Pad to choose your game and press START to launch!", max_height=y // 9, editable=False)
+        self.InstructText = self.add(InstructText, name="Instructions", value = "Just select your game and press START to launch!", max_height=y // 8, editable=False)
         self.Browser = self.add(Browser, name="Available ROMs", editable=True)
 
     def exit_func(self, _input):
+        curses.beep()
         exit(0)
 
 pyNES = App()
